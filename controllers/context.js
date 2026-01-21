0225fb4880064e1615d644cb3ea41ab0949e47cf
“CindyS” <veggiebyte@yahoo.com>
2026-01-21 10:11:16 -0800

    Add placeholder context controller, set up .env

---
+++ controllers/context.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+// Placeholder - will build this later
+router.get('/', (req, res) => {
+  res.json({ message: 'Context route - coming soon' });
+});
+
+module.exports = router;
